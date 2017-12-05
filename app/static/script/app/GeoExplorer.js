a658f164335b84e7751595c7d52036dd2105b981
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2017-12-05 14:53:18 +0100

    Merge with OL 2.13.1

--- app/static/script/app/GeoExplorer.js
+++ app/static/script/app/GeoExplorer.js
@@ -35,7 +35,7 @@ Ext.override(Ext.dd.DragTracker, {
     Ext.preg("gx_wmssource", gxp.plugins.WMSSource);
     Ext.preg("gx_olsource", gxp.plugins.OLSource);
     Ext.preg("gx_googlesource", gxp.plugins.GoogleSource);
-    Ext.preg("gx_bingsource", gxp.plugins.BingSource);
+    // Ext.preg("gx_bingsource", gxp.plugins.BingSource);
     Ext.preg("gx_osmsource", gxp.plugins.OSMSource);
     // use layermanager instead of layertree
     Ext.preg("gxp_layertree", gxp.plugins.LayerManager);
